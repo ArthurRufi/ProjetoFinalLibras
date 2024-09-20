1b34e243df50521fdb20cfcae69303819c7bf7aa
ArthurRufi <temparthur87@gmail.com>
2024-09-20 16:09:09 -0300

    tabelas curso e sinal criadas

--- libras/curso/models.py
+++ libras/curso/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Curso(models.Model):
+
+    nome = models.CharField()
+    idcurso = models.CharField()
+    #desenvolver logica para atualizacao da quantidade de sinais
+    quantidadeDeSinais = models.IntegerField()
+    graduacao = models.CharField()
+
+    class Meta:
+        verbose_name = ("Curso")
+        verbose_name_plural = ("Cursos")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Curso_detail", kwargs={"pk": self.pk})
--- libras/interpretes/models.py
+++ libras/interpretes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Interprete(models.Model):
+
+    nome = models.CharField()
+    idInterprete = models.CharField()
+    #desenvolver logica para criacao da logica para pesquisa do sinal do interprete no banco de dados!!!!!
+    sinalInterprete = models.CharField()
+
+    class Meta:
+        verbose_name = _("Interprete")
+        verbose_name_plural = _("Interpretes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Interprete_detail", kwargs={"pk": self.pk})
