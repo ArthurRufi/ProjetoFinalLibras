ea4a6b2c613032298fd18d671886a934ca2ee5b9
ArthurRufi <temparthur87@gmail.com>
2024-10-03 08:53:54 -0300

    config

--- libras/libras/settings.py
+++ libras/libras/settings.py
@@ -144,15 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'paginaprincipal/static/paginaprincipal/css'),
-                    #os.path.join(BASE_DIR, 'paginaprincipal/static/paginaprincipal/images'),
-                   # os.path.join(BASE_DIR, 'paginaprincipal/static/paginaprincipal/js'),
-                    # os.path.join(BASE_DIR, 'paginaprincipal/static/paginaprincipal/utils'),
-                    
-
-                    
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
